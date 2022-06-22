a9a63efce3f1f9ab34088f76cacaa749399a99fb
Andrea Prina <and.prina@gmail.com>
2022-06-22 16:22:28 +0200

    Rimossa descrizione

--- js/main.js
+++ js/main.js
@@ -26,13 +26,6 @@ if (userInputWord === userReversedWord){
 
 
 
-// ! Pari e Dispari
-// ! L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
-// ! Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
-// ! Sommiamo i due numeri
-// ! Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
-// ! Dichiariamo chi ha vinto.
-
 // *** ESERCIZIO 2 - Pari o Dispari ***
 
 // Ask the user if they want to choose EVEN or ODD
