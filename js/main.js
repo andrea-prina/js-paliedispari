8a63521fa9872588322503c01cdad89cf24ac8c2
Andrea Prina <and.prina@gmail.com>
2022-06-22 15:51:54 +0200

    Esercizio 1 - Completato

--- js/main.js
+++ js/main.js
@@ -1,11 +1,34 @@
-// Palidroma
-// Chiedere all’utente di inserire una parola.
-// Creare una funzione per capire se la parola inserita è palindroma.
+// *** ESERCIZIO 1 - Parola palindroma ***
+
+// Ask the user to input a word
+const userInputWord = prompt("Inserisci la parola da verificare");
+
+// Reverse it by iterating through it's characters from finish to start and adding them to a new string variable
+function reverseWord (inputWord){
+    
+    outputWord = "";
+    for (let i = inputWord.length - 1; i >= 0; i--){
+        outputWord += inputWord[i];
+    }
+
+    return outputWord;
+}
+
+const userReversedWord = reverseWord(userInputWord);
+
+// Check if the original word is the same of the reversed one
+
+if (userInputWord === userReversedWord){
+    console.log(`La parola ${userInputWord} è palindroma`);
+} else {
+    console.log(`La parola ${userInputWord} NON è palindroma`);
+}
 
 
-// Pari e Dispari
-// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
-// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
-// Sommiamo i due numeri
-// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
-// Dichiariamo chi ha vinto.
+
+// ! Pari e Dispari
+// ! L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// ! Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// ! Sommiamo i due numeri
+// ! Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// ! Dichiariamo chi ha vinto.
